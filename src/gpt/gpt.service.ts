b3b19932414ffabbd0a25a77ee6ef053af094b2b
MrPloman <polplanaayet@gmail.com>
2024-11-17 21:02:55 -0600

    implemnted first corrector

--- src/gpt/gpt.service.ts
+++ src/gpt/gpt.service.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { ortographyCheckUseCase } from './use-cases/index';
 import { OrtographyDTO } from './dtos/ortography.dto';
+import OpenAI from 'openai';
 
 @Injectable()
 export class GptService {
+  private openAiConfig = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
   public async ortographyCheck(ortographyDto: OrtographyDTO) {
-    return await ortographyCheckUseCase({ prompt: ortographyDto.prompt });
+    return await ortographyCheckUseCase(this.openAiConfig, {
+      prompt: ortographyDto.prompt,
+    });
   }
 }
--- src/gpt/use-cases/ortography.use-case.ts
+++ src/gpt/use-cases/ortography.use-case.ts
@@ -1,10 +1,44 @@
+import OpenAI from 'openai';
 interface InterfaceOptions {
   prompt: string;
 }
 
-export const ortographyCheckUseCase = async (options: InterfaceOptions) => {
+export const ortographyCheckUseCase = async (
+  openai: OpenAI,
+  options: InterfaceOptions,
+) => {
   const { prompt } = options;
-  return {
-    prompt,
-  };
+
+  const completion = await openai.chat.completions.create({
+    model: 'gpt-3.5-turbo-1106',
+    max_tokens: 100,
+    messages: [
+      {
+        role: 'assistant',
+        content: `You are a language, syntax and vocabulary corrector. 
+        You have to detect by yourself which language was written and your aim is to correct all misspelled words and syntax issues from the prompt was given.
+        Rate the text out of 100. And collect all the misspelled words and show the correct way inside an array of string. Return teh response in json format.
+        Example of format:
+        {
+                userScore: number,
+                errors: string[],
+                message_checked: string
+        
+        }
+        `,
+      },
+      {
+        role: 'user',
+        content: prompt,
+      },
+    ],
+    response_format: {
+      type: 'json_object',
+    },
+  });
+  console.log(completion.choices[0].message.content);
+
+  const jsonResp = JSON.parse(completion.choices[0].message.content);
+
+  return jsonResp;
 };
