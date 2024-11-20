ceb6fd7b165de8494cc2e3e9d6a3718e8e76db0f
MrPloman <polplanaayet@gmail.com>
2024-11-19 19:34:49 -0600

    Accurated system in the prompt

--- src/gpt/use-cases/ortography.use-case.ts
+++ src/gpt/use-cases/ortography.use-case.ts
@@ -10,19 +10,22 @@ export const ortographyCheckUseCase = async (
   const { prompt } = options;
 
   const completion = await openai.chat.completions.create({
-    model: 'gpt-3.5-turbo-1106',
-    max_tokens: 100,
+    model: 'gpt-3.5-turbo',
     messages: [
       {
         role: 'assistant',
-        content: `You are a language, syntax and vocabulary corrector. 
-        You have to detect by yourself which language was written and your aim is to correct all misspelled words and syntax issues from the prompt was given.
-        Rate the text out of 100. And collect all the misspelled words and show the correct way inside an array of string. Return teh response in json format.
+        content: `You are a language, syntax and vocabulary corrector. You have to give the response in the same language the prompt was given, this is mandatory.
+        You have to detect by yourself which language was written and your aim is to correct all misspelled words and syntax issues from the prompt was given, that correction has to be located inside the variable message_checked.
+        Rate the text out of 100 and put the result inside userScore variable.
+        Collect all the misspelled words and put it them inside a variable, show the correct way they are supposed to be written inside that variable string array called errors. 
+        At the same time you have to return a message giving your impresions about the rating of the message was given. On that message try to be kind and informal, using emojis. Put that message inside a variable called review.
+        Return the response in json format.
         Example of format:
         {
                 userScore: number,
                 errors: string[],
-                message_checked: string
+                message_checked: string,
+                review: string
         
         }
         `,
@@ -36,7 +39,6 @@ export const ortographyCheckUseCase = async (
       type: 'json_object',
     },
   });
-  console.log(completion.choices[0].message.content);
 
   const jsonResp = JSON.parse(completion.choices[0].message.content);
 
